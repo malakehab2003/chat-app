9d2a3bdcede2cd06baaf6952c8ac737fb0317a69
malakehab2003 <lokaehab2003@gmail.com>
2024-07-15 21:46:57 +0300

    add token to login

--- Front-end/src/backend/login.jsx
+++ Front-end/src/backend/login.jsx
@@ -6,11 +6,23 @@ const instance = axios.create({
 });
 
 export const SignInRequest = async (email, pass) => {
-	console.log(email, pass);
+	try {
+		console.log(email, pass);
 
-	const res = await instance.post('signIn', {
-		email,
-		pass,
-	});
-	console.log(res);
+		const res = await instance.post('signIn', {
+			email,
+			pass,
+		});
+		console.log(res);
+
+		let { token } = res.data;
+    token = `Bearer ${token}`;
+    console.log(token);
+
+    if (token) {
+      localStorage.setItem('token', token);
+    }
+	} catch (err) {
+		console.error('Error during sign-in:', err);
+	}
 };
